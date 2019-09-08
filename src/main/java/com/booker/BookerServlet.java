562679aad4eba2c9be101b96ddc68332b88072e3
mchozhang <vivamcho@gmail.com>
2019-09-08 15:28:58 +1000

    add com.booker

--- src/main/java/com/booker/BookerServlet.java
+++ src/main/java/com/booker/BookerServlet.java
@@ -1,3 +1,5 @@
+package com.booker;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "BookerServlet")
+@WebServlet(name = "com.booker.BookerServlet")
 public class BookerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("处理Get请求。。。。。");
