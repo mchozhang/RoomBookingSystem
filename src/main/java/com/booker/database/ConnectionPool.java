ef5fa25ec4ac063113172a2f242b810f74ff20f3
mchozhang <vivamcho@gmail.com>
2019-09-13 01:19:21 +1000

    add new database

--- src/main/java/com/booker/database/ConnectionPool.java
+++ src/main/java/com/booker/database/ConnectionPool.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 
 public class ConnectionPool {
     private static final String driver = "org.postgresql.Driver";
-//    private static final String username = "ywvhywkzeausaz";
-//    private static final String password = "12976fe4015a30b27a136dd1efb9c0b0fba90f84cfd1f3cf0d490590d7c7d498";
-//    private static final String dbUrl = "jdbc:postgres://ywvhywkzeausaz:12976fe4015a30b27a136dd1efb9c0b0fba90f84cfd1f3cf0d490590d7c7d498@ec2-184-73-232-93.compute-1.amazonaws.com:5432/d18gkho6mnfpet";
+    private static final String username = "vyzvffinhzurwi";
+    private static final String password = "cc95d3e41b8bd02016b90adead770b9e3ce02362f118d19d8a21cd3e1d627fa5";
+    private static final String dbUrl = "jdbc:postgresql://ec2-107-20-168-237.compute-1.amazonaws.com:5432/dfmoitgrtekd1u";
 
-    private static final String username = "postgres";
-    private static final String password = "admin";
-    private static final String dbUrl = "jdbc:postgresql://localhost:5433/postgres";
+//    private static final String username = "postgres";
+//    private static final String password = "admin";
+//    private static final String dbUrl = "jdbc:postgresql://localhost:5433/postgres";
 
     private ConnectionPool(){
     }
